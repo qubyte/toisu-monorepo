637bf8d19d8169332417bfbcda25f34a83266eeb
Mark S. Everitt <mark.s.everitt@gmail.com>
2015-12-20 11:36:29 +0000

    Handle throws from the first middleware in a chain.

--- index.js
+++ index.js
@@ -3,20 +3,17 @@
 function runner(req, res, middlewares) {
   var context = this;
 
-  function wrapper(index) {
-    var middleware = middlewares && middlewares[index];
+  function makeWrapper(index) {
+    return function wrapper() {
+      var middleware = middlewares && middlewares[index];
 
-    if (!middleware || res.headersSent || !res.writable) {
-      return Promise.resolve();
-    }
-
-    return Promise.resolve(middleware.call(context, req, res))
-      .then(function () {
-        return wrapper(index + 1);
-      });
+      if (middleware && !res.headersSent && res.writable) {
+        return Promise.resolve(middleware.call(context, req, res)).then(makeWrapper(index + 1));
+      }
+    };
   }
 
-  return wrapper(0);
+  return Promise.resolve().then(makeWrapper(0));
 }
 
 module.exports = runner;
