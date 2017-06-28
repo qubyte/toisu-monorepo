d4063f3adae48c48645e279d29c3cb03c07fba77
Mark S. Everitt <mark.s.everitt@gmail.com>
2017-06-28 21:22:31 +0100

    Make tests use mocha.

--- test/lib/parseGeneric.tests.js
+++ test/lib/parseGeneric.tests.js
@@ -1,114 +1,97 @@
 'use strict';
 
-var tape = require('tape');
+var assert = require('assert');
 var sinon = require('sinon');
 var parseGeneric = require('../../lib/parseGeneric');
 
-tape('parseGeneric', function (t) {
-  t.test('is a function', function (t) {
-    t.equal(typeof parseGeneric, 'function');
-    t.end();
+describe('parseGeneric', function () {
+  it('is a function', function () {
+    assert.equal(typeof parseGeneric, 'function');
   });
 
-  t.test('returns a promise', function (t) {
-    t.ok(parseGeneric('req', 'res', 'options') instanceof Promise, 'instanceof Promise');
-    t.end();
+  it('returns a promise', function () {
+    assert.ok(parseGeneric('req', 'res', 'options', function () {}, new Map()) instanceof Promise, 'instanceof Promise');
   });
 
-  t.test('calls the given parseFunction with req, res, options, and a callback', function (t) {
+  it('calls the given parseFunction with req, res, options, and a callback', function () {
     var parseFunction = sinon.stub();
+
     parseGeneric('req', 'res', 'options', parseFunction, new Map());
 
-    t.equal(parseFunction.args[0][0], 'req', 'req');
-    t.equal(parseFunction.args[0][1], 'res', 'res');
-    t.equal(parseFunction.args[0][2], 'options', 'options');
-    t.equal(typeof parseFunction.args[0][3], 'function', 'callback');
-    t.end();
+    assert.equal(parseFunction.args[0][0], 'req', 'req');
+    assert.equal(parseFunction.args[0][1], 'res', 'res');
+    assert.equal(parseFunction.args[0][2], 'options', 'options');
+    assert.equal(typeof parseFunction.args[0][3], 'function', 'callback');
   });
 
-  t.test('defaults options to an empty object', function (t) {
+  it('defaults options to an empty object', function () {
     var parseFunction = sinon.stub();
+
     parseGeneric('req', 'res', undefined, parseFunction, new Map());
 
-    t.equal(parseFunction.args[0][0], 'req', 'req');
-    t.equal(parseFunction.args[0][1], 'res', 'res');
-    t.deepEqual(parseFunction.args[0][2], {}, 'defaults options to {}');
-    t.equal(typeof parseFunction.args[0][3], 'function', 'callback');
-    t.end();
+    assert.equal(parseFunction.args[0][0], 'req', 'req');
+    assert.equal(parseFunction.args[0][1], 'res', 'res');
+    assert.deepEqual(parseFunction.args[0][2], {}, 'defaults options to {}');
+    assert.equal(typeof parseFunction.args[0][3], 'function', 'callback');
   });
 
-  t.test('when the parseFunction calls back with an error', function (t) {
-    t.test('it rejects the parseGeneric promise with the error', function (t) {
+  describe('when the parseFunction calls back with an error', function () {
+    it('rejects the parseGeneric promise with the error', function () {
       var parseFunction = sinon.stub();
       var error = new Error();
       var promise = parseGeneric('req', 'res', 'options', parseFunction, new Map());
 
       parseFunction.yield(error);
 
-      promise.then(
+      return promise.then(
         function () {
-          t.fail('did not reject');
-          t.end();
+          throw new Error('did not reject');
         },
         function (e) {
-          t.equal(e, error, 'rejects with the error');
-          t.end();
+          assert.equal(e, error, 'rejects with the error');
         }
       );
     });
 
-    t.test('it does not set the body property on the context', function (t) {
+    it('does not set the body property on the context', function () {
       var parseFunction = sinon.stub();
-      var error = new Error();
       var context = { set: sinon.stub() };
       var promise = parseGeneric('req', 'res', 'options', parseFunction, context);
 
-      parseFunction.yield(error);
+      parseFunction.yield(new Error());
 
-      promise.then(
+      return promise.then(
         function () {
-          t.fail('did not reject');
-          t.end();
+          throw new Error('did not reject');
         },
         function () {
-          t.equal(context.set.callCount, 0, 'does not set the body');
-          t.end();
+          assert.equal(context.set.callCount, 0, 'does not set the body');
         }
       );
     });
   });
 
-  t.test('when the parseFunction calls back with the body', function (t) {
-    t.test('it resolves the parseGeneric promise', function (t) {
+  describe('when the parseFunction calls back with the body', function () {
+    it('resolves the parseGeneric promise', function () {
       var parseFunction = sinon.stub();
       var promise = parseGeneric('req', 'res', 'options', parseFunction, new Map());
 
       parseFunction.yield(null, 'the-body');
 
-      promise
-        .then(function () {
-          t.end();
-        });
+      return promise;
     });
 
-    t.test('it sets the body on the context', function (t) {
+    it('sets the body on the context', function () {
       var parseFunction = sinon.stub();
       var context = { set: sinon.stub() };
       var promise = parseGeneric('req', 'res', 'options', parseFunction, context);
 
       parseFunction.yield(null, 'the-body');
 
-      promise.then(
-        function () {
-          t.equal(context.set.callCount, 1, 'called once');
-          t.deepEqual(context.set.args[0], ['body', 'the-body'], 'sets the body');
-          t.end();
-        },
-        function () {
-          t.fail('rejected');
-          t.end();
-        }
-      );
+      return promise.then(function () {
+        assert.equal(context.set.callCount, 1, 'called once');
+        assert.deepEqual(context.set.args[0], ['body', 'the-body'], 'sets the body');
+      });
     });
   });
 });
